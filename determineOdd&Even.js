21b27c4b5fa09c599a45c83ad0f7ced1997c89f5
ahmedhimel21 <ahmedhimel000@gmail.com>
2024-03-12 01:54:40 +0600

    determineOdd&Even.js

---
+++ determineOdd&Even.js
@@ -0,0 +1,21 @@
+// while loop: odd number
+let i = 1;
+while(i <= 10){
+  // console.log(i);
+  i += 2;
+}
+// for loop: odd number
+for(let x = 1; x <=10; x +=2){
+  // console.log(x);
+}
+
+// while loop: odd number
+let y = 0;
+while(y <= 10){
+  // console.log(y);
+  y += 2;
+}
+// for loop: odd number
+for(let z = 0; z <=10; z +=2){
+  console.log(z);
+}
