58c3962ad9998500ba68bbe70d56831121913dff
ahmedhimel21 <ahmedhimel000@gmail.com>
2024-03-12 01:06:58 +0600

    largestNumberWithCondition.js

---
+++ largestNumberWithCondition.js
@@ -0,0 +1,13 @@
+const numberOne = 89;
+const numberTwo = 98;
+const numberThree = 96;
+
+if(numberOne > numberTwo && numberOne > numberThree){
+  console.log('NumberOne is the largest number');
+}
+else if(numberTwo > numberOne && numberTwo > numberThree){
+  console.log('NumberTwo is the largest number');
+}
+else{
+  console.log('NumberThree is the largest number');
+}
