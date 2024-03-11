f0f74399e01194af6b62cd7034c5deacb0eeb104
ahmedhimel21 <ahmedhimel000@gmail.com>
2024-03-12 02:00:38 +0600

    readArrayValue.js

---
+++ readArrayValue.js
@@ -0,0 +1,14 @@
+let members = ['Anik', 'Sayid', 'Rubel','Humayon'];
+// using while loop
+let i = 0;
+while(i < members.length){
+  let element = members[i];
+  // console.log(element);
+  i++;
+}
+
+// using for loop
+for(let x =0; x <members.length; x++){
+  let element = members[x];
+  console.log(x,element);
+}
