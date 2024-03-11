5a76c37ab50fd0620d8f9859cbd9e14f9e14d771
ahmedhimel21 <ahmedhimel000@gmail.com>
2024-03-12 00:51:59 +0600

    comparision.js

---
+++ compasrision.js
@@ -0,0 +1,37 @@
+// return true or false
+
+// gaterThen: condition true
+let gaterThen = 5 > 4;
+// gaterThen: condition false
+let gaterThen1 = 5 > 6
+console.log(gaterThen, gaterThen1);
+
+// lessThen: condition true
+let lessThen = 5 < 6;
+// lessThen> condition false
+let lessThen1 = 5 < 4;
+console.log(lessThen, lessThen1);
+
+// gaterThenEqual: condition true
+let gaterThenEqu = 7 >= 6;
+let gaterThenEqu1 = 5 >= 5;
+console.log(gaterThenEqu, gaterThenEqu1);
+// gaterThenEqual: condition false
+let gaterThenEqu2 = 8 >= 9;
+console.log(gaterThenEqu2);
+
+// lessThenEqual: condition true
+let lessThenEqu = 5 <= 6
+let lessThenEqu1 = 5 <= 5;
+// lessThenEqual: condition false
+let lessThenEqu2 = 5 <= 4;
+console.log(lessThenEqu,lessThenEqu1,lessThenEqu2);
+
+// not equal: condition true
+let notEqu = 5 !== 4
+// not equal: condition false
+let notEqu1 = 5 !== 5;
+console.log('Not Equal:',notEqu,notEqu1);
+
+
+
