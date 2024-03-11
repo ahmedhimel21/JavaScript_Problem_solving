854f0abcba506cc19ec1c8937ffd91fc97e6e829
ahmedhimel21 <ahmedhimel000@gmail.com>
2024-03-12 02:37:53 +0600

    switch.js

---
+++ switch.js
@@ -0,0 +1,14 @@
+let color = 'red';
+switch(color){
+  case 'green':
+    console.log('It is green');
+    break;
+  case 'yellow':
+    console.log('It is yellow');
+    break;
+  case 'purple':
+    console.log('It is purple');
+    break;
+  default:
+    console.log('It is black');
+}
