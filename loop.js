77bcb368587fd29090455a562c3f62f965428929
ahmedhimel21 <ahmedhimel000@gmail.com>
2024-03-12 01:40:50 +0600

    loop.js

---
+++ loop.js
@@ -0,0 +1,21 @@
+// show 1 to 10 as output using while loop
+let i =1;
+while(i <= 10){
+  // console.log(i);
+  i++
+}
+// show 1 to 10 as output using for loop
+for(let y = 1; y <= 10; y++){
+  // console.log(y);
+}
+
+// show 10 to 1 as output using while loop
+let x = 10;
+while(x >= 1){
+  // console.log(x);
+  x--;
+}
+// show 10 to 1 as output using for loop
+for(let z=10; z >= 1; z--){
+  // console.log(z);
+}
