730bbd04102b150b08547e9d1e5dc267376aa123
ahmedhimel21 <ahmedhimel000@gmail.com>
2024-03-12 00:59:47 +0600

    If-else.js

---
+++ if-else.js
@@ -0,0 +1,15 @@
+// and operator (&&): If all condition true then if statement will be executed.
+if(5 > 4 && 4 < 5){
+  console.log('Condition true');
+}
+else{
+  console.log('Condition false');
+}
+
+// or operator: If any of the condition true then if statement will be executed
+if(9 > 8 || 8 !== 8){
+  console.log('Condition true');
+}
+else{
+  console.log('Condition false');
+}
